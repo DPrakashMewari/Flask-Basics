d7223faf7e4008e284058c59e95e910d04335352
D Prakash <54624250+DPrakashMewari@users.noreply.github.com>
2021-09-03 14:12:21 +0530

    Add files via upload

--- Saleflask/app.py
+++ Saleflask/app.py
@@ -1,42 +1,64 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
-import con
 from con import *
 
+mail = Mail(app)
 
 class New(db.Model):
     id = db.Column('Product_id', db.Integer, primary_key=True)
     Product = db.Column(db.String(100))
-    Qty = db.Column(db.Float(50))
+    barcode = db.Column(db.String(50))
+    Stock = db.Column(db.Float(50))
     Price = db.Column(db.Integer)
 
-
-    def __init__(self, Product, Qty, Price):
+    def __init__(self,Product,barcode,Stock,Price):
         self.Product = Product
-        self.Qty = Qty
+        self.barcode= barcode
+        self.Stock = Stock
         self.Price = Price
 
 
 
-@app.route('/')
-def list_employees():
-    return render_template('list_employees.html', Employees=New.query.all())
 
+@app.route("/")
+def homepage():
+    return render_template('index.html')
 
-@app.route('/add', methods=['GET', 'POST'])
-def addEmployee():
+@app.route("/prod")
+def productpage():
+    return render_template('Products.html',Product=New.query.all())
+
+@app.route('/addproduct', methods=['GET', 'POST'])
+def addProduct():
     if request.method == 'POST':
-        if not request.form['Product'] or not request.form['Qty'] or not request.form['Price']:
-            flash('Please enter all the fields', 'error')
-        else:
-            employee = New(request.form['Product'], request.form['Qty'],request.form['Price'])
+        Product = request.form['Product']
+        barcode = request.form['barcode']
+        Stock = request.form['Stock']
+        Price = request.form['Price']
+        data = New(Product=Product,barcode=barcode,Stock=Stock,Price=Price)
+        db.session.add(data)
+        db.session.commit()
+        flash("Stock Updated")
+        return redirect(url_for('homepage'))
+    return render_template('Products.html')
 
-            db.session.add(employee)
-            db.session.commit()
-            flash('Record was successfully added')
-            return redirect(url_for('list_employees'))
-    return render_template('add.html')
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
+def delete(id):
+    my_data = New.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    msg = Message('subject', sender='Prakash.mewari@gmail.com', recipients=['Prakash.mewari@gmail.com'])
+    flash("Product Sell.Also Please Check You Mail")
+    msg.body = "Product Successfully Sell by Admin"
+    mail.send(msg)
 
 
-if __name__ == '__main__':
+
+    return redirect(url_for('productpage'))
+
+
+
+
+
+if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True)
+    app.run(debug=True,port=4000)
